8799c4e7c9274dd2285334aee64d3532a11d9298
sephy18 <sephy18@qq.com>
2016-06-17 00:37:49 +0800

    update gift

--- assets/js/game1.js
+++ assets/js/game1.js
@@ -10,7 +10,7 @@
 			iWidth: 40,
 			bMgLeft: 10,
 			bMgRight: 10,
-			bMgBottom: 105,
+			bMgBottom: 85,
 			bWidth: 100,
 			perfectB: 20,
 			greatB: 10,
@@ -110,11 +110,14 @@
 						return;
 					}
 				} else {
-					var src1 = $('.box').css('backgroundImage');
-					var src2 = src1.replace('gif', 'png');
-					$('.box').css('backgroundImage', src2);
+					
+					if(!$('.box').html()) {
+						$('.box').append('<div class="box-miss"></div>');
+					}
 					setTimeout(function() {
-						$('.box').css('backgroundImage', src1);
+						if($('.box').html()) {
+							$('.box').html('');
+						}
 					}, 1000);
 					var missSpd  = ( (options.bMgBottom) / (options.wHeight - options.iWidth - options.bMgBottom) ) * thisSpd / 2;
 					missCount--;
