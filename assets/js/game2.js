e893ebd688072979cd70d59deb542bbd76c63e54
sephy18 <sephy18@qq.com>
2016-06-25 14:09:41 +0800

    update game2 core

--- assets/js/game2.js
+++ assets/js/game2.js
@@ -1,201 +1,203 @@
-// JavaScript Document
-$(function() {
-	
-	var iWidth = 20;
-	var bMgBottom = 30;
-	var bWidth = 60;
-	var perfectB = 20;
-	var greatB = 15;
-	var fineB = 10
-	var target = [0, 800, 900, 1000];
-	var fallSpd = [0, 800, 800, 800];
-	
-	var wWidth = $(window).width();
-	var wHeight = $(window).height();
-	
-	var t, iCount, score, missCount, diff, tCount, level;
+(function ($) {
 
-	orient();
-	init();
+	var bonusidnum, totalbonus, score, level, isMute;
 	
-	$('.start').on('mouseup', function() {
-		$('.start, .gameover, .gameclear').hide();
-		level = 1;
-		resetData();
-		loadGame();
-	});
-	$('.next').on('mouseup', function() {
-		$('.next, .gameclear').hide();
-		$('.gift').animate({bottom: '-250px'}, 500);
-		setTimeout(function() {
-			level++;
-			resetData();
-			loadGame();
-		}, 500)
-	});
-	$('.box').swipe( {
-		swipeStatus:function(event, phase, direction, distance, duration, fingers, fingerData, currentDirection)
-		{
-			if (phase == 'start') {
-				diff = event.targetTouches[0].pageX - $(this).offset().left;
-			}
-			if (phase == 'start' || phase == 'move') {
-				$('.box').css('left', event.targetTouches[0].pageX - diff + 'px');
-			}
-		},
-		threshold: 1,
-		maxTimeThreshold: null,
-		fingers: 1
-	});
-	$(window).on( 'orientationchange', function(e){
-		orient();
-	}); 
-	function init() {
-		var html = '<div class="bar">SCORE: <span class="score">0</span></div>';
-		html += '<div class="lv">LEVEL <span class="level">1</span></div>';
-		html += '<div class="start">START</div>';
-		html += '<div class="next">NEXT</div>';
-		html += '<div class="gameclear">GAME CLEAR</div>';
-		html += '<div class="gameover">GAMEOVER<br/>SCORE: <span class="goscore"></span></div>';
-		html += '<div class="box"></div>';
-		$('.container').append(html);
-		$('.start').delay(500).fadeIn(300);
-	}
-	function loadGame() {
-		$('.box').css('left', (wWidth - bWidth) / 2 + 'px').fadeIn(500);
-		setTimeout(createItem, 2000);
-	}
-	function resetData() {
-		iCount = 0;
-		tCount = 0;
-		score = 0;
-		diff = 0;
-		$('.score').html(score);
-		$('.level').html(level);
-	}
-	function createItem() {
-		var html = '<div class="item item' + randItem() + '" id="i' + toId(iCount) + '"></div>';
-		$('.container').append(html);
-		var $this = $('#i' + toId(iCount));
-		$this.css('left', randX());
-		var thisSpd = randFallSpd();
-		$this.animate({'top': wHeight - iWidth - bMgBottom + 'px'}, {duration: thisSpd, easing: 'easeInSine', complete: function() {
-			test($this);
-		}});
-	}
-	function test($this) {
-		var iP = $this.offset().left + iWidth / 2;
-		var bP = $('.box').offset().left + bWidth/ 2;
-		var bL = $('.box').offset().left;
-		var bR = $('.box').offset().left + bWidth;
+	$.fn.game2 = function(options) {
 		
-		if (iP > bL && iP < bR) {
-				
-			var offsetX = Math.max((iP - bP) * 10, 1);
-			var cur = Math.abs(offsetX / 1000);
-			//console.log(offsetX + ' ' + cur);
-			var bool = new Parabola({
-				el: $this,
-				offset: [offsetX, 0],
-				curvature: 0.01,
-				duration: 1000,
-				callback:function(){
-					test($this);
-				},
-				stepCallback:function(x,y){
-					
-					if(x < -bP || x + bP > wWidth - iWidth/2) {
-						console.log(x + bP);
-						console.log('stp');
-						bool.stop();
-					}
-				}
-			});
-			bool.start();
+		options = $.extend({
+			timertime: 90,
+			defaulttimertime: 90,
+			boxnum: 5,
+			bonusidnum
+			//wWidth: $(window).width(),
+			//wHeight: $(window).height()
+		}, options);
+		
+		init();
+		
+		$('.start_btn').click(function() {
+			$('.finalscore').hide().html('');
+			$(this).hide();
+			$('.bonuslist').html('');
+			totalbonus = bonusidnum= 0;
+			$('#scoreboard').text('S: ' + totalbonus);
 			
-			$('.score').html(score);
-			if(score > target[level]) {
-				clearTimeout(t);
-				clearGame();
-				return;
+			gengamebox();
+			while (nosameexist()) {
+				alertpopup();
+				resetgamebox();
+			}		
+			//set timer
+			timertime = defaulttimertime;
+			$('#timer').text(showtime(timertime));
+			$('#progressbar').progressbar({value: timertime / defaulttimertime * 100});
+			
+			$timeinterval = setInterval(function(){
+				timertime--;
+				$('#timer').text(showtime(timertime));
+				$('#progressbar').progressbar({value: Math.round(timertime / defaulttimertime * 100)});
+				if (timertime < 0) {
+					$('#timer').text(showtime(0));
+					clearInterval($timeinterval);
+					$('.activebox').removeClass('activebox').addClass('inactivebox');
+					$('.finalscore').html('YOUR SCORE<br />' + totalbonus).fadeIn(500);
+					$('.start_btn').delay(500).fadeIn(500);
+				}
+			}, 1000);
+		});	
+			
+		$(document).on('click', '.activebox', function() {
+			var theEvent = window.event || arguments.callee.caller.arguments[0];	var x = theEvent.pageX - 50,
+					y = theEvent.pageY - 20;
+			//mark samebox
+			$samecount = 1;
+			$(this).addClass('counted').addClass('checked').addClass('samecolor');
+			$samecountnew = boxcheck($(this), $samecount, 1);
+			while ($samecountnew > $samecount) {
+				$samecount = $samecountnew;
+				$('.samecolor').each(function() {
+					if(!$(this).hasClass('checked')) {
+						$samecountnew = boxcheck($(this), $samecountnew, 1);
+						$(this).addClass('checked');
+					}
+				});
 			}
-		} else {
-			var missSpd  = 50;
-			$this.animate({'top': wHeight - iWidth + 'px'}, missSpd, 'linear', function() {
-				$this.animate({'opacity': '0'}, 500);
-				setTimeout(function() {
-					$this.remove();
-				}, 500);
+			//succeed
+			if($samecount > 2) {
+				//score & bonus
+				timertime+=0.3;
+				var bonus = Math.round(($samecount - 1 + Math.pow(($samecount - 2), 1.2)) * 10);
+				$position = 'left:' + x + 'px;top:' + y + 'px';
+				$('.bonuslist').append('<div class="bonus" id="bonus' + bonusidnum + '">+' + bonus + '</div>');
+				$bonusid = '#bonus' + bonusidnum;
+				$($bonusid).attr('style', $position).show().delay(500).effect('drop', { direction: "right" });
+				bonusidnum++;
+				totalbonus += bonus;
+				$('#scoreboard').text('S: ' + totalbonus);
+				//explode & generate
+				$('.samecolor').each(function() {
+					$boxr = parseInt($(this).attr('id').substr(1, 1));
+					$boxc = parseInt($(this).attr('id').substr(3, 1));							
+					$(this).removeClass('samecolor').effect('explode', 400).remove();
+					var i, j;
+					j = $boxr - 1;
+					for(i = j; i > -1; i--) {
+						$boxid = '#r' + i + 'c' + $boxc;
+						var k = i + 1;
+						$newid = 'r' + k + 'c' + $boxc;
+						$($boxid).attr('id', $newid).attr('style', "position: -38px").animate({top: "0px"}, rantime());
+					}	
+					$colId = '#c' + $boxc;
+					$($colId).prepend('<li class="box box' + rancolor() + ' original activebox" id=r'+ 0 + 'c' + $boxc + '>' + 0 + $boxc + '</li>');
+					$newboxid = '#r' + 0 + 'c' + $boxc;
+					$($newboxid).animate({top: "0px"}, rantime());
+				});	
+				//check same
+				while(nosameexist()) {
+					alertpopup();
+					resetgamebox();
+				}
+			}
+			//fail	
+			else {
+				timertime-=0.3;
+			}	
+			//reset
+			$('.samecolor').removeClass('samecolor');
+			$('.counted').removeClass('counted');
+			$('.checked').removeClass('checked');
+		});
+		function init() {
+			bonusidnum = 0;
+			totalbonus = 0;
+			boxnum = options.boxnum;
+			timertime = options.timertime;
+			defaulttimertime = options.defaulttimertime;
+			$("#progressbar").progressbar({value: 100});
+		}
+		
+		function alertpopup() {
+			$('.alertbox').show().delay(1000).fadeOut(200);
+		}
+		function rancolor() {
+			return Math.floor(Math.random()*boxnum);
+		}
+		function rantime() {
+			return Math.floor(Math.random()*300) + 150;
+		}
+		function gengamebox() {
+			$('#game').html('');
+			var i, j;
+			for (i = 0; i < 10; i++) {
+				$('#game').append('<li class="boxcolumn"><ul id="c' + i + '"></ul></li>');
+				for (j = 0; j < 10; j++) {
+					$colId = '#c' + i;
+					$($colId).append('<li class="box box' + rancolor() + ' original activebox" id=r'+ j + 'c' + i + '>' + j + i + '</li>');
+					$newboxid = '#r' + j + 'c' + i;
+					$($newboxid).animate({top: "0px"}, rantime());
+				}
+			}
+		}
+		function resetgamebox() {
+			$('#game').html('');
+			var i, j;
+			for (i = 0; i < 10; i++) {
+				$('#game').append('<li class="boxcolumn"><ul id="c' + i + '"></ul></li>');
+				for (j = 0; j < 10; j++) {
+					$colId = '#c' + i;
+					$($colId).append('<li class="box box' + rancolor() + ' activebox" id=r'+ j + 'c' + i + '>' + j + i + '</li>');
+					$newboxid = '#r' + j + 'c' + i;
+				}
+			}
+		}
+		function nosameexist() {
+			var count = 0;
+			$('.activebox').each(function() { 
+				if(boxcheck($(this), 1, 2) > 2) {count++;}
 			});
-			finishGame();
+			if(count == 0) return true;
+		}
+		function showtime($timertime) {
+			$minute = Math.floor($timertime / 60);
+			$second = Math.round($timertime - $minute * 60);
+			$second = $second > 9 ? $second : '0' + $second;
+			return $minute + ':' + $second;
+		}
+		function comparecolor($boxcolor, $row, $col, $samecount) {
+			$newboxid = '#r' + $row + 'c' + $col;
+			if((!$($newboxid).hasClass('counted'))&&($($newboxid).attr('class').substr(4, 4) == $boxcolor)) {
+				$($newboxid).addClass('samecolor').addClass('counted');
+				$samecount++;
+			}
+			return $samecount;
+		}	
+		function comparecolor2($boxcolor, $row, $col, $samecount) {
+			$newboxid = '#r' + $row + 'c' + $col;
+			if(($($newboxid).attr('class').substr(4, 4) == $boxcolor)) {
+				$samecount++;
+			}
+			return $samecount;
+		}	
+		function boxcheck($box, $samecount, $method) {
+			$boxcolor = $box.attr('class').substr(4, 4);
+			$boxr = parseInt($box.attr('id').substr(1, 1));
+			$boxc = parseInt($box.attr('id').substr(3, 1));
+			switch ($method) {
+				case 1:
+					if($boxc > 0) $samecount = comparecolor($boxcolor, $boxr, $boxc - 1, $samecount);	
+					if($boxc < 9) $samecount = comparecolor($boxcolor, $boxr, $boxc + 1, $samecount);	
+					if($boxr > 0) $samecount = comparecolor($boxcolor, $boxr - 1, $boxc, $samecount);	
+					if($boxr < 9) $samecount = comparecolor($boxcolor, $boxr + 1, $boxc, $samecount);	
+					break;
+				case 2:
+					if($boxc > 0) $samecount = comparecolor2($boxcolor, $boxr, $boxc - 1, $samecount);	
+					if($boxc < 9) $samecount = comparecolor2($boxcolor, $boxr, $boxc + 1, $samecount);	
+					if($boxr > 0) $samecount = comparecolor2($boxcolor, $boxr - 1, $boxc, $samecount);	
+					if($boxr < 9) $samecount = comparecolor2($boxcolor, $boxr + 1, $boxc, $samecount);	
+					break;
+			}
+			return $samecount;
 		}
 	}
-	function clearGame() {
-		$('.tip, .item').stop(true).remove();
-		$('.box').hide();
-		if (level < target.length - 1) {
-			$('.gameclear').html('LEVEL ' + level + '<br/>CLEAR').fadeIn(300);
-			var html = '<div class="gift" id="gift' + level + '"></div>';
-			$('.container').append(html);
-			$('#gift' + level).delay(600).animate({bottom: '-180px'}, 1000).delay(300).animate({bottom: '-220px'}, 70).delay(100).animate({bottom: 0}, 300).animate({bottom: '-20px'}, 50, function() {
-				setTimeout(function() {
-					$('.next').fadeIn(300);
-				}, 1000);
-			});
-		} else {
-			$('.gameclear').html('GAME CLEAR').show();
-			$('.start').html('RETRY').show();
-		}
-	}
-	function finishGame() {
-		$('.tip, .item').stop(true).remove();
-		$('.box').hide();
-		$('.goscore').html(score);
-		$('.gameover').show();
-		$('.start').html('RETRY').show();
-	}
-	function appendTip(rank) {
-		$('.tip').stop(true).remove();
-		var html = '<div class="tip tip-' + rank + '" id="t' + toId(tCount) + '">' + rank.toUpperCase() + '</div>';
-		$('.container').append(html);
-		var $this = $('#t' + toId(tCount));
-		$this.fadeIn(200).delay(400).fadeOut(200);
-		tCount++;
-		setTimeout(function() {
-			$this.remove();
-		}, 800);
-	}
-	function toId(num) {
-		if (num < 10) {
-			num = '00' + num;
-		} else if (num < 100) {
-			num = '0' + num;
-		}
-		return num;
-	}
-	function randTime() {
-		return Math.floor(Math.random() * 500) + createSpd[level];
-	}
-	function randItem() {
-		return Math.floor(Math.random() * 3);
-	}
-	function randX() {
-		return (Math.floor(Math.random() * (wWidth - iWidth)) + iWidth / 2) + 'px';
-	}
-	function randFallSpd() {
-		return Math.floor(Math.random() * 500) + fallSpd[level];
-	}
-})
-
-function orient() {
-	if (window.orientation == 0 || window.orientation == 180) {
-		$("body").attr("class", "portrait");
-		orientation = 'portrait';
-		return false;
-	}
-	else if (window.orientation == 90 || window.orientation == -90) {
-		$("body").attr("class", "landscape");
-		orientation = 'landscape';
-		return false;
-	}
-}	
+	
+})(jQuery);
